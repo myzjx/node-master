0cd7005f1068331e14b1883b660f7529917aee55
“myzjx” <my_zjx@outlook.com>
2019-10-04 17:15:42 +0800

    +++

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const Koa = require('koa')
 // 注意require('koa-router')返回的是函数:
-const router = require('koa-router')()
+const bodyParser = require('koa-bodyparser')
+const Router = require('koa-router')
 const app = new Koa()
+const router = new Router()
+app.use(bodyParser())
 
-// 添加路由
-router.get('/', async (ctx, next) => {
-  ctx.response.body = `<h1>index page</h1>`
-})
-
-router.get('/home', async (ctx, next) => {
-  ctx.response.body = `<h1>home page</h1>`
-})
-
-router.get('/404', async (ctx, next) => {
-  ctx.response.body = `<h1>404 Not Found</h1>`
+router.get('/users/:id', async (ctx, next) => {
+  ctx.response.body = '<h1>user page</h1>'
+  ctx.user = {
+    id: 3,
+    name: 'xiaoming'
+  }
+  await next(),
+    async (ctx) => {
+      console.log(ctx.user)
+    }
 })
 // 调用路由中间件
 app.use(router.routes())
