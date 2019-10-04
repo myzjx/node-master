7fcf1a83b3ee60946a7ab69f7e22cd7771abc604
“myzjx” <my_zjx@outlook.com>
2019-10-04 14:20:14 +0800

    +++

--- middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('../routers');
-const http = require('./plugins/http');
-const login = require('./plugins/login');
-
-module.exports = (app) => {
-  app.use(http());
-  app.use(login());
-  app.use(router());
-}
