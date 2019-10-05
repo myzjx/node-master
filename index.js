8c1493d2401890b8a87a75fac202a1915b7eedd4
“myzjx” <my_zjx@outlook.com>
2019-10-05 14:42:17 +0800

    +++

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 const router = require('./router')
+const middleware = require('./middleware')
 
 app.use(bodyParser())
 
+middleware(app)
 router(app)
 
 app.listen(3000, () => {
