44e1ea96f723c5e4b60a8fe781d747f1aeedc91c
“myzjx” <my_zjx@outlook.com>
2019-10-05 14:58:32 +0800

    +++

--- middleware/log/logger.js
+++ middleware/log/logger.js
@@ -2,21 +2,25 @@ const log4js = require('log4js')
 
 module.exports = () => {
   return async (ctx, next) => {
+    const start = Date.now()
     log4js.configure({
       appenders: {
-        cheese: {
+        appInfo: {
           type: 'file',
           filename: 'logger.log'
         }
       },
       categories: {
         default: {
-          appenders: ['cheese'],
+          appenders: ['appInfo'],
           level: 'info'
         }
       }
     })
-    const logger = log4js.getLogger('cheese')
+    const logger = log4js.getLogger('appInfo')
     await next()
+    const end = Date.now()
+    const responseTime = end - start;
+    logger.info(`响应时间为${responseTime/1000}s`);
   }
 }
