ec6eb7db7457e1898009e62b7739d04c132aea82
“myzjx” <my_zjx@outlook.com>
2019-10-05 15:33:35 +0800

    +++

--- middleware/log/index.js
+++ middleware/log/index.js
@@ -1,5 +1,16 @@
 const logger = require('./logger')
 
-module.exports = () => {
-  return logger()
+module.exports = (opt) => {
+  const loggerMiddleware = logger(opt)
+  return (ctx, next) => {
+    return loggerMiddleware(ctx, next)
+      .catch((e) => {
+        if (ctx.status < 500) {
+          ctx.status = 500
+        }
+        ctx.log.error(e.stack)
+        ctx.state.logged = true
+        ctx.throw(e)
+      })
+  }
 }
--- middleware/log/logger.js
+++ middleware/log/logger.js
@@ -2,34 +2,50 @@ const log4js = require('log4js')
 const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
 
 // 默认公共参数对象
-// const baseInfo = {
-//   appLogLevel: 'debug', // 指定记录的日志级别
+const baseInfo = {
+  appLogLevel: 'debug', // 指定记录的日志级别
+  dir: 'logs', // 指定日志存放的目录名
+  env: 'dev', // 指定当前环境，当为开发环境时，在控制台也输出，方便调试
+  projectName: 'api_serve', // 项目名，记录在日志中的项目信息
+}
 
-// }
+const {
+  env,
+  appLogLevel,
+  dir
+} = baseInfo
 
 module.exports = () => {
   const contextLogger = {}
-  log4js.configure({
-    appenders: {
-      cheese: {
-        type: 'dateFile', // 日志类型 
-        filename: `logs/task`, // 输出的文件名
-        pattern: '-yyyy-MM-dd.log', // 文件名增加后缀
-        alwaysIncludePattern: true // 是否总是有后缀名
-      }
-    },
+  const appenders = {}
+
+  appenders.cheese = {
+    type: 'dataFile',
+    filename: `${dir}/task`,
+    pattern: '-yyyy-MM-dd.log',
+    alwaysIncludePattern: true
+  }
+
+  // 环境变量为dev local development 认为是开发环境
+  if (env === "dev" || env === "local" || env === "development") {
+    appenders.out = {
+      type: "console"
+    }
+  }
+  let config = {
+    appenders,
     categories: {
       default: {
-        appenders: ['cheese'],
-        level: 'info'
+        appenders: Object.keys(appenders),
+        level: appLogLevel
       }
     }
-  })
-
+  }
   const logger = log4js.getLogger('cheese')
 
   return async (ctx, next) => {
     const start = Date.now()
+    log4js.configure(config)
     methods.forEach((method, i) => {
       contextLogger[method] = (message) => {
         logger[method](message)
