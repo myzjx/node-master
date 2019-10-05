0c0a60b13bb838be2fed1b7b0cbfdf36dd91a4c2
“myzjx” <my_zjx@outlook.com>
2019-10-05 15:39:28 +0800

    +++

--- middleware/log/index.js
+++ middleware/log/index.js
@@ -1,16 +1,5 @@
 const logger = require('./logger')
 
-module.exports = (opt) => {
-  const loggerMiddleware = logger(opt)
-  return (ctx, next) => {
-    return loggerMiddleware(ctx, next)
-      .catch((e) => {
-        if (ctx.status < 500) {
-          ctx.status = 500
-        }
-        ctx.log.error(e.stack)
-        ctx.state.logged = true
-        ctx.throw(e)
-      })
-  }
+module.exports = () => {
+  return logger()
 }
